764c0ed3c12795cefde5f1da929f8d25a7fecdef
LenTheDev <lhl7@njit.edu>
2021-05-04 04:58:46 -0400

    "Feature: Database Model with the Service Repository Pattern"

---
+++ Entities/Configuration/SectionConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Entities.Configuration
+{
+    public class SectionConfiguration : IEntityTypeConfiguration<Section>
+    {
+        public void Configure(EntityTypeBuilder<Section> builder)
+        {
+            builder.HasData
+            (
+                new Section
+                {
+                    Id = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991872"),
+                    SectionName = "012",
+                },
+                new Section
+                {
+                    Id = new Guid("3d490a70-94tretre5-9494-524fertr2ce2"),
+                    SectionName = "Sectioncsfffcc
+                }t
+            );
+        }
+    }
+}
---
+++ Entities/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Course
+    {
+        [Column("CourseId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Course name is a required field.")]
+        [MaxLength(15, ErrorMessage = "Must begin with department ID followed by course number.")]
+        public string CourseName { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
