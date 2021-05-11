18bf30ee1f232d95fbbdb3bfa3385156986729b2
LenTheDev <lhl7@njit.edu>
2021-05-11 08:13:04 -0400

    "Feature: Validation"

--- Entities/DataTransferObjects/UserForCreationDto.cs
+++ Entities/DataTransferObjects/UserForCreationDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
+//DTO
 namespace Entities.DataTransferObjects
 {
     public class UserForCreationDto
